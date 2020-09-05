39b658af7589c6ae517dde3656d33441660898f3
sfespinosa <scottjames.espinosa@gmail.com>
2020-09-05 14:35:25 -0700

    display friends feed

--- app/serializers/establishment_collection_serializer.rb
+++ app/serializers/establishment_collection_serializer.rb
@@ -1,5 +1,5 @@
 class EstablishmentCollectionSerializer < ActiveModel::Serializer
-  attributes :id, :user_comments, :visited, :us_state_id, :map_marker
+  attributes :id, :user_comments, :visited, :us_state_id, :map_marker, :establishment
   has_one :user_id
-  has_one :establishment
+  has_one :establishment_id
 end
